2c946fc69b2b6180355f93f779a125f809f5bf38
macdougalljs <macdojer@sheridancollege.ca>
2019-07-17 20:07:21 -0400

    Determined how to randomize the array of cards

--- src/Solo1/GamePlay.java
+++ src/Solo1/GamePlay.java
@@ -5,6 +5,10 @@
  */
 package Solo1;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 /**
  *
  * @author Jeremiah MacDougall
@@ -12,12 +16,21 @@ package Solo1;
 public class GamePlay {
     public static void main(String[]args) 
     {
-    Deck ch = new Deck();
-    ch.generate(); //calls method to generate 60 cards
+    Deck origch = new Deck();
+    origch.generate(); //calls method to generate 60 cards
+    
+    List<Deck> list = Arrays.asList(origch);
+    Collections.shuffle(list);
+    Object[] ch = list.toArray();
+    
+    
     // print 60 cards
+    /* this is the original code to display the cards sequentally. 
     for (Cards c :ch.deck) // ch.deck is the first data, stored into c
     {
         System.out.println(c.getS()+ " "+ c.getV());
+
     }
+*/
     }
 }
